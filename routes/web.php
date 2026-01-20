e96da12fd8554713997ed940bf5cc303364ca125
MPProgrammer <mariusz@codelarix.dev>
2026-01-20 23:51:29 +0100

    refactor: clean up web route definitions
    - Add strict types declaration
    - Remove boilerplate comments
    - Reorder use statements
    - Add descriptive comments for each route

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+declare(strict_types=1);
+
 use App\Http\Controllers\PetController;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+// List page
 Route::get('/', [PetController::class, 'index'])->name('pets.index');
 
+// Create page
 Route::get('/pets/create', [PetController::class, 'create'])->name('pets.create');
+// @todo Add docblock for create method
+
+// Store a new pet
 Route::post('/pets', [PetController::class, 'store'])->name('pets.store');
 
+// Edit a pet
 Route::get('/pets/{id}/edit', [PetController::class, 'edit'])->name('pets.edit');
+
+// Update a pet
 Route::post('/pets/{id}', [PetController::class, 'update'])->name('pets.update');
 
+// Delete a pet
 Route::post('/pets/{id}/delete', [PetController::class, 'destroy'])->name('pets.destroy');
+
+// Delete a pet (ajax)
 Route::post('/ajax/pets/{id}/delete', [PetController::class, 'destroyAjax'])->name('pets.destroy.ajax');
 
+// Get all pets (ajax)
 Route::get('/ajax/pets', [PetController::class, 'ajaxList']);
