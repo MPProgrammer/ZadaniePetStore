d222498b982ccdb266100c2f86f25e07b562e15e
MPProgrammer <mariusz@codelarix.dev>
2026-01-20 23:51:46 +0100

    refactor: enhance service with strict types and docs
    - Add strict types for properties and methods
    - Improve code clarity with detailed docblocks
    - Add internal comments for better

--- app/Http/Services/PetstoreService.php
+++ app/Http/Services/PetstoreService.php
@@ -1,69 +1,142 @@
 <?php
 
+/**
+ * @author    Panek Mariusz <mariusz@codelarix.dev>
+ * @copyright Copyright © Codelarix (https://codelarix.dev)
+ * @version   1.0.0
+ */
+
+declare(strict_types=1);
+
 namespace App\Http\Services;
 
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
 class PetstoreService
 {
-    private $baseUrl;
+    /** @var string The base URL */
+    private string $baseUrl;
+
+    /** @var string The API key */
     private $apiKey;
 
+    /**
+     * Class constructor.
+     */
     public function __construct()
     {
+        // Set the base URL for the API
         $this->baseUrl = config('services.petstore.url');
+
+        // Set the API key
         $this->apiKey = config('services.petstore.key');
     }
 
-    private function client()
+    /**
+     * Get a new instance of the HTTP client with the API key set as a header.
+     *
+     * @return PendingRequest The HTTP client instance
+     */
+    private function client(): PendingRequest
     {
-        return Http::withHeaders([
+        // Set the API key, Accept header to application/json.
+        $headers = [
             'api_key' => $this->apiKey,
-            'Accept' => 'application/json'
-        ]);
+            'Accept'  => 'application/json'
+        ];
+
+        // Return the HTTP client with the headers set.
+        return Http::withHeaders($headers);
     }
 
-    public function getPet($id)
+    /**
+     * Get a pet by its ID.
+     *
+     * @param int|string $id The pet ID
+     *
+     * @return Response The HTTP response
+     */
+    public function getPet(int|string $id): Response
     {
-        return $this->client()->get("$this->baseUrl/pet/$id");
+        // Send a GET request to the /pet/{id} endpoint.
+        return $this->client()->get($this->baseUrl . "/pet/" . $id);
     }
 
-    public function findByStatus(string $status)
+    /**
+     * Find pets by their status.
+     *
+     * @param string $status The pet status
+     *
+     * @return Response The HTTP response
+     */
+    public function findByStatus(string $status): Response
     {
+        // Send a GET request to the /pet/findByStatus endpoint with the status parameter set.
         return $this->client()->get(
             $this->baseUrl . '/pet/findByStatus',
-            ['status' => $status]
+            [
+                'status' => $status
+            ]
         );
     }
 
-    public function createPet(array $data)
+    /**
+     * Create a new pet in the petstore.
+     *
+     * @param string $name The name of the pet to be created
+     * @param string $status The status of the pet to be created
+     *
+     * @return Response The HTTP response
+     */
+    public function createPet(string $name, string $status): Response
     {
+        // Send a POST request to the /pet endpoint with the name and status set.
+        // The photoUrls parameter is required by the API, but it is not used in this case.
         return $this->client()->post(
             $this->baseUrl . '/pet',
             [
-                // 'id' => $data['id'],
-                'name' => $data['name'],
-                'status' => $data['status'],
-                'photoUrls' => [],
+                'name'      => $name,
+                'status'    => $status,
+                'photoUrls' => []
             ]
         );
     }
 
-    public function updatePet(array $data)
+    /**
+     * Update a pet in the petstore.
+     *
+     * @param int|string $id The pet ID
+     * @param string $name The name of the pet to be updated
+     * @param string $status The status of the pet to be updated
+     *
+     * @return Response The HTTP response
+     */
+    public function updatePet(int|string $id, string $name, string $status): Response
     {
+        // Send a PUT request to the /pet endpoint with the pet ID, name, and status set.
         return $this->client()->put(
             $this->baseUrl . '/pet',
             [
-                'id' => $data['id'],
-                'name' => $data['name'],
-                'status' => $data['status'],
-                'photoUrls' => [],
+                'id'        => $id,
+                'name'      => $name,
+                'status'    => $status,
+                'photoUrls' => []
             ]
         );
     }
 
-    public function deletePet(int $id)
+    /**
+     * Delete a pet from the petstore.
+     *
+     * @param int|string $id The pet ID
+     *
+     * @return Response The HTTP response
+     */
+    public function deletePet(int|string $id): Response
     {
+        // Send a DELETE request to the /pet/{id} endpoint to delete a pet.
         return $this->client()->delete(
             $this->baseUrl . '/pet/' . $id
         );
